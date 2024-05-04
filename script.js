b7c11c17e05e330c0c832954cb0e301b1251724e
pozzitivy-show-tech <enpozzitivy@gmail.com>
2024-05-04 18:46:48 +0100

    Added box

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let navElement = document.querySelectorAll("nav ul li");
+let boxButton = document.querySelector("#box-icon");
+let boxDropdown = document.querySelector(".box-dropdown-part");
 let dropdown = document.querySelectorAll(".dropdown-part");
+let body = document.querySelector("body");
 
 function showDropdown(event) {
     hideDropdown();
@@ -9,19 +12,36 @@ function showDropdown(event) {
     switch (navID) {
         case "phone":
             document.querySelector("#phone-dropdown").style.display = "flex";
+            body.classList.add("disable-scroll");
             break;
         case "hands":
             document.querySelector("#hands-dropdown").style.display = "flex";
+            body.classList.add("disable-scroll");
             break;
         case "sound":
             document.querySelector("#sound-dropdown").style.display = "flex";
+            body.classList.add("disable-scroll");
             break;
         case "accessories":
             document.querySelector("#accessories-dropdown").style.display = "flex";
+            body.classList.add("disable-scroll");
             break;
     
     }
+
 }
+
+function boxDisplay(){
+    if (boxDropdown.style.display == "flex"){
+        boxDropdown.style.display = "none";
+    }
+    else{
+        boxDropdown.style.display = "flex";
+    }
+    body.classList.toggle("disable-scroll");
+}
+
+boxButton.addEventListener("click",boxDisplay)
 
 for (let x = 0; x < dropdown.length; x++) {
     dropdown[x].addEventListener("mouseleave", hideDropdown);
@@ -31,6 +51,7 @@ for (let x = 0; x < dropdown.length; x++) {
 function hideDropdown() {
     for (let i = 0; i < dropdown.length; i++) {
         dropdown[i].style.display = "none";
+        body.classList.remove("disable-scroll");
       }
 }
 
