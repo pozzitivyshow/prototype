2ca2b230c94b978e07c78dfab46e26eb78be06df
pozzitivy-show-tech <enpozzitivy@gmail.com>
2024-05-04 14:01:41 +0100

    Finished dropdown mechanism

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-let navElement = document.querySelectorAll("nav ul li a");
-let dropdown = document.querySelector(".dropdown-part");
+let navElement = document.querySelectorAll("nav ul li");
+let dropdown = document.querySelectorAll(".dropdown-part");
 
-dropdown.addEventListener("mouseenter", showDropdown);
-dropdown.addEventListener("mouseleave", hideDropdown);
+function showDropdown(event) {
+    hideDropdown();
+    let nav = event.currentTarget;
+    let navID = nav.getAttribute("id");
+    console.log(navID);
+    switch (navID) {
+        case "phone":
+            document.querySelector("#phone-dropdown").style.display = "flex";
+            break;
+        case "hands":
+            document.querySelector("#hands-dropdown").style.display = "flex";
+            break;
+        case "sound":
+            document.querySelector("#sound-dropdown").style.display = "flex";
+            break;
+        case "accessories":
+            document.querySelector("#accessories-dropdown").style.display = "flex";
+            break;
+    
+    }
+}
 
-function showDropdown() {
-    dropdown.style.display = "flex";
-    dropdown.style.justifyContent = "space-between";
+for (let x = 0; x < dropdown.length; x++) {
+    dropdown[x].addEventListener("mouseleave", hideDropdown);
+    
 }
 
 function hideDropdown() {
-    dropdown.style.display = "none";
+    for (let i = 0; i < dropdown.length; i++) {
+        dropdown[i].style.display = "none";
+      }
 }
 
 for (let i = 0; i < navElement.length; i++) {
